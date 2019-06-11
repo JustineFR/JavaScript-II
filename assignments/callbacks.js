c862366ef11218792d2b4fbf1eaee89106c25e6a
Justine Gennaro <justine.rouzier@gmail.com>
2019-06-11 16:38:49 -0500

    callback challeng 1 and 2 done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -26,6 +26,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
+  return cb(arr.getLength)
   // getLength passes the length of the array into the callback.
 }
 
